abaf66d0c14b43d2e62d6c0cf1ac59150d22948f
Ribaar <124412755+Ribaar@users.noreply.github.com>
2023-02-16 20:47:03 +0700

    1

--- kontol.js
+++ kontol.js
@@ -1,8 +1,25 @@
-var boxContainer = document.getElementsByTagName("a");
-boxContainer.forEach(
-    function cekurl(element) {
-        if (this.href === window.location.href) {
-            this.classList.add("active");
-        }
+$('.center').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
     }
-)
+  ]
+});
