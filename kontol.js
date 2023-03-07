758f5cfbe87a2ce494ddf772736a352203c83182
Ribaar <124412755+Ribaar@users.noreply.github.com>
2023-03-07 12:32:34 +0700

    1

--- kontol.js
+++ kontol.js
@@ -1,7 +1,12 @@
-$('.center').slick({
+$('.slider).slick({
   centerMode: true,
   centerPadding: '60px',
   slidesToShow: 3,
+  infinite: true,
+  autoplay: true,
+  speedautoplay: 300,
+  focusonselect: true,
+  variablewidth: true,
   responsive: [
     {
       breakpoint: 768,
